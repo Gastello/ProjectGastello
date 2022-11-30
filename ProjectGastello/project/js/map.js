a2f560788255306fc5160cf69dbffeb33278583d
Gastello <maximgst@gmail.com>
2022-12-01 01:11:28 +0200

    com

--- ProjectGastello/project/js/map.js
+++ ProjectGastello/project/js/map.js
@@ -1,20 +1,20 @@
-let header__burger = document.querySelector('.header__burger');
+let header_burger = document.querySelector('.header__burger');
 let header_menu = document.querySelector('.header__menu');
 let back = document.querySelector('body');
-let header__list = document.querySelector('.header__list');
+let header_invisible = document.querySelector('.header__invisible');
 
-console.log(header_menu)
-header__burger.onclick = function () {
-   header__burger.classList.toggle('active');
+header_burger.onclick = function () {
+   header_burger.classList.toggle('active');
    header_menu.classList.toggle('active');
    back.classList.toggle('lock');
 }
 
-header_menu.onclick = function (event) {
-   console.log('click');
+header_menu.onclick = closeMap;
+header_invisible.onclick = closeMap;
+
+function closeMap(event) {
    if (event.target === event.currentTarget) {
-      console.log('Handle click');
-      header__burger.classList.remove('active');
+      header_burger.classList.remove('active');
       header_menu.classList.remove('active');
       back.classList.remove('lock');
    }
