ed0b07078f685e19b5bf88b6c67607742096b9d2
Gastello <maximgst@gmail.com>
2022-11-30 02:35:27 +0200

    com

--- ProjectGastello/project/js/map.js
+++ ProjectGastello/project/js/map.js
@@ -3,9 +3,19 @@ let header_menu = document.querySelector('.header__menu');
 let back = document.querySelector('body');
 let header__list = document.querySelector('.header__list');
 
+console.log(header_menu)
 header__burger.onclick = function () {
    header__burger.classList.toggle('active');
    header_menu.classList.toggle('active');
    back.classList.toggle('lock');
 }
 
+header_menu.onclick = function (event) {
+   console.log('click');
+   if (event.target === event.currentTarget) {
+      console.log('Handle click');
+      header__burger.classList.remove('active');
+      header_menu.classList.remove('active');
+      back.classList.remove('lock');
+   }
+}
