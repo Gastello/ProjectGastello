acd9e732908858fce5723c6131d468df78b90ee4
Gastello <maximgst@gmail.com>
2022-12-12 15:54:00 +0200

    com

---
+++ ProjectGastello/project/js/slider.js
@@ -0,0 +1,23 @@
+const swiper_2 = new Swiper('.swiper_2', {
+   nextButton: '.swiper__next',
+   loop: false,
+   effect: 'slide',
+   speed: 0,
+}
+
+);
+
+let swiperNextArr = document.querySelectorAll('.swiper__next');
+let swiperPrevArr = document.querySelectorAll('.swiper__prev');
+
+for (let swiperNext of swiperNextArr) {
+   swiperNext.onclick = function () {
+      swiper_2.slideNext();
+   }
+}
+
+for (let swiperPrev of swiperPrevArr) {
+   swiperPrev.onclick = function () {
+      swiper_2.slidePrev();
+   }
+}
