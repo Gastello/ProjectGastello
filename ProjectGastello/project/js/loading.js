034a02d3a9f8620ef75db0a1e4deadeb56e2cf9d
Gastello <maximgst@gmail.com>
2022-12-18 13:58:46 +0200

    com

--- ProjectGastello/project/js/loading.js
+++ ProjectGastello/project/js/loading.js
@@ -1,10 +1,14 @@
 function onReady(callback) {
-   var intervalId = window.setInterval(function () {
-      if (document.getElementsByTagName('body')[0] !== undefined) {
-         window.clearInterval(intervalId);
-         callback.call(this);
-      }
-   }, 500);
+   Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
+
+
+      var intervalId = window.setInterval(function () {
+         if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalId);
+            callback.call(this);
+         }
+      }, 500);
+   });
 }
 
 function setVisible(selector, visible) {
