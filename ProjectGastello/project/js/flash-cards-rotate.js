f0f83c6e0882ad0b0e449faa2024ae6768406ff0
Gastello <maximgst@gmail.com>
2023-03-01 19:48:32 +0200

    flash-cards fixes

--- ProjectGastello/project/js/flash-cards-rotate.js
+++ ProjectGastello/project/js/flash-cards-rotate.js
@@ -1,6 +1,8 @@
-let flashCards = document.querySelectorAll('#flash-card-rotate'); 
+let flashCards = document.querySelectorAll('.flash-card'); 
 for (const flashCard of flashCards) {
-   flashCard.onclick = () => { 
-      flashCard.parentElement.parentElement.parentElement.classList.toggle('active');
+   flashCard.onclick = (e) => {  
+      if(e.target.className == 'flash-card__gear_inner') return;
+      flashCard.classList.toggle('active');
    }  
 }
+
--- ProjectGastello/project/js/flash-cards-slider.js
+++ ProjectGastello/project/js/flash-cards-slider.js
@@ -1,13 +1,32 @@
 const swiper_4 = new Swiper('.swiper_4', {
-   effect: 'flip', 
+   effect: 'flip',
    allowTouchMove: true,
    navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
    },
-   breakpoints: { 
+   flipEffect: {
+      slideShadows: false,
+   },
+   breakpoints: {
       649.98: {
          allowTouchMove: false,
-      }, 
-  }
+      },
+   }
 });
+
+document.body.onkeyup = function (e) {
+   if (e.code == "Space" || e.keyCode == 32) {
+      let activeSlide = document.querySelector('.swiper-slide-active');
+      let activeFlashCard = activeSlide.querySelector('.flash-card');
+      activeFlashCard.classList.toggle('active');
+   }
+
+   if (e.code == "ArrowLeft" || e.keyCode == 37) {
+      swiper_4.slidePrev();
+   }
+
+   if (e.code == "ArrowRight" || e.keyCode == 39) {
+      swiper_4.slideNext();
+   }
+}
