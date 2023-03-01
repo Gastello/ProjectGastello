46bf02ce90729f59173f4d12424e260c88b048af
Gastello <maximgst@gmail.com>
2023-03-01 19:14:05 +0200

    fullscreen added

---
+++ ProjectGastello/project/js/fullscreen.js
@@ -0,0 +1,30 @@
+let isFullscreen = false;
+let logo = document.querySelector('.header__logo');
+let elem = document.documentElement;
+logo.onclick = () => {
+    isFullscreen ? closeFullscreen() : openFullscreen();
+}
+
+/* View in fullscreen */
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    isFullscreen = true;
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    isFullscreen = false;
+}
