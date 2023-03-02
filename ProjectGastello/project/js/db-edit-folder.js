0c1307d05834c8df3be9ef1258bbaf68f98659ef
Gastello <maximgst@gmail.com>
2023-03-02 21:27:44 +0200

    db-edit-folder complete

--- ProjectGastello/project/js/db-edit-folder.js
+++ ProjectGastello/project/js/db-edit-folder.js
@@ -1,30 +1,44 @@
 let folderEditBtns = document.querySelectorAll('.db-folder__edit');
 
 for (const btn of folderEditBtns) {
-    btn.onclick = (e) => {
+    btn.onclick = () => {
         let folder = btn.parentElement.parentElement;
         let folderSaveBtn = folder.querySelector('.db-folder__save');
         let folderEditBtn = folder.querySelector('.db-folder__edit');
         let folderDeleteBtn = folder.querySelector('.db-folder__delete');
+        let folderNameContainer = folder.querySelector('.db-folder__title'); 
+        let folderName = folderNameContainer.textContent.replaceAll(/\s+/g,' ').trim();
+        let folderNameContainerHeight = folderNameContainer.offsetHeight; 
         folderSaveBtn.style.display = 'block';
         folderEditBtn.style.display = 'none';
         folderDeleteBtn.style.display = 'none';
-        folder.onclick = null;
-        // folder.onclick = checkIsNotNav;
+        folderNameContainer.style.display = 'none'; 
+        folderNameContainer.insertAdjacentHTML(
+            'beforebegin',
+            `<textarea class="db-folder__textarea">${folderName}</textarea>`
+        )
+        let folderTextarea = folder.querySelector('.db-folder__textarea');
+        folderTextarea.style.height = `${folderNameContainerHeight}px`;
+        folder.onclick = null; 
     }
 }
 
 let folderSaveBtns = document.querySelectorAll('.db-folder__save');
 
 for (const btn of folderSaveBtns) {
-    btn.onclick = (e) => {
+    btn.onclick = () => {
         let folder = btn.parentElement.parentElement;
         let folderSaveBtn = folder.querySelector('.db-folder__save');
         let folderEditBtn = folder.querySelector('.db-folder__edit');
         let folderDeleteBtn = folder.querySelector('.db-folder__delete');
+        let folderTextarea = folder.querySelector('.db-folder__textarea');
+        let folderNameContainer = folder.querySelector('.db-folder__title');
+        folderNameContainer.textContent = folderTextarea.value.replaceAll(/\s+/g,' ').trim();  
+        folderTextarea.remove();
         folderSaveBtn.style.display = 'none';
         folderEditBtn.style.display = 'block';
         folderDeleteBtn.style.display = 'block';
+        folderNameContainer.style.display = 'block';
         folder.onclick = checkIsNotNav;
     }
 }
